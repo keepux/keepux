b35f9767870dfadf19e928a066624c8a26d43edc
Michael Neal <iammichaelneal@gmail.com>
2018-03-20 18:52:18 -0500

    disableHostCheck devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,53 @@
 const path = require('path');
 
 module.exports = {
-	context: __dirname,
-	entry: {
-		main: ['./src/js/ClientApp.jsx']
-	},
-	devtool: 'cheap-eval-source-map',
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	devServer: {
-		publicPath: '/public/',
-		historyApiFallback: true
-	},
-	resolve: {
-		extensions: ['.js', '.jsx', '.json']
-	},
-	stats: {
-		colors: true,
-		reasons: true,
-		chunks: true
-	},
-	module: {
-		rules: [
-			{
-				enforce: 'pre',
-				test: /\.jsx?$/,
-				loader: 'eslint-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.jsx?$/,
-				loader: 'babel-loader'
-			},
-			{
-				test: /\.sass?$/,
-				loader: 'style-loader!css-loader!sass-loader',
-				include: path.join(__dirname, 'src', 'styles')
-			},
-			{
-				test: /\.png$$/,
-				loader: 'file-loader'
-			},
-			{
-				test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-				loader: 'file-loader'
-			}
-		]
-	}
+    context: __dirname,
+    entry: {
+        main: ['./src/js/ClientApp.jsx']
+    },
+    devtool: 'cheap-eval-source-map',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        publicPath: '/public/',
+        historyApiFallback: true,
+        disableHostCheck: true
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    stats: {
+        colors: true,
+        reasons: true,
+        chunks: true
+    },
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.jsx?$/,
+                loader: 'eslint-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.sass?$/,
+                loader: 'style-loader!css-loader!sass-loader',
+                include: path.join(__dirname, 'src', 'styles')
+            },
+            {
+                test: /\.png$$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                loader: 'file-loader'
+            }
+        ]
+    }
 };
